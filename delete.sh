ac14b46cf94819e3b6137d6c5ad6de476ab3c330
Omer Tuchfeld <otuchfel@redhat.com>
2021-06-01 15:45:51 +0300

    scc

--- delete.sh
+++ delete.sh
@@ -6,4 +6,10 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 NAME=mymirror
 
-oc delete -n gpu-operator-resources deployment/${NAME} route/${NAME} service/${NAME} pvc/${NAME} serviceaccount/${NAME}
+oc delete -n gpu-operator-resources \
+    deployment/${NAME} \
+    route/${NAME} \
+    service/${NAME} \
+    pvc/${NAME} \
+    serviceaccount/${NAME} \
+    securitycontextconstraint/${NAME}
