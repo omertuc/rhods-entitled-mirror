4c2d4747e01901ec60ec6196034fef97991c384e
Omer Tuchfeld <otuchfel@redhat.com>
2021-06-01 10:46:40 +0300

    Omit storage class to be cloud agnostic

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+pushd "${SCRIPT_DIR}"
+
+podman build -f Containerfile.sync . -t quay.io/otuchfel/rhods-mirror-sync:latest
+podman build -f Containerfile.serve . -t quay.io/otuchfel/rhods-mirror-serve:latest
+
+echo "Done"
--- upload.sh
+++ upload.sh
@@ -6,9 +6,6 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 pushd "${SCRIPT_DIR}"
 
-podman build -f Containerfile.sync . -t quay.io/otuchfel/rhods-mirror-sync:latest
-podman build -f Containerfile.serve . -t quay.io/otuchfel/rhods-mirror-serve:latest
-
 podman push quay.io/otuchfel/rhods-mirror-sync:latest
 podman push quay.io/otuchfel/rhods-mirror-serve:latest
 
