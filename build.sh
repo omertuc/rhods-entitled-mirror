58fd51600a8df9ef98a921b1181d2c4aaf7ad29b
Omer Tuchfeld <otuchfel@redhat.com>
2021-06-04 22:50:26 +0300

    Reorganization, client auth

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 pushd "${SCRIPT_DIR}"
 
-podman build -f Containerfile.sync . -t quay.io/otuchfel/rhods-mirror-sync:latest
-podman build -f Containerfile.serve . -t quay.io/otuchfel/rhods-mirror-serve:latest
+podman build -f Containerfile.sync containers/sync/ -t quay.io/otuchfel/rhods-mirror-sync:latest
+podman build -f Containerfile.serve containers/serve/ -t quay.io/otuchfel/rhods-mirror-serve:latest
 
 echo "Done"
--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,13 @@ set -euxo pipefail
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-oc process -f ${SCRIPT_DIR}/openshift/templates/mirror.yaml -p NAME=mymirror -p NAMESPACE=rhods-mirror | oc apply -f -
+if ! oc get nodes -o json | jq '.items | .[] | .metadata.labels.entitled' -r -e > /dev/null; then
+    echo "Please entitle one of the cluster nodes and give it an entitled=true label"
+    exit 1
+fi
+
+oc process -f ${SCRIPT_DIR}/openshift/template.yaml \
+    -p NAME=mymirror \
+    -p NAMESPACE=rhods-mirror \
+    -p "CLIENT_CA=$( < ./auth/ca/generated_ca.crt base64 --wrap=0 )" \
+    | oc apply -f -
